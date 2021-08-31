829d260b296ae5030cfe5a77b835e21515938c93
ftanevski <tanevski.f98@gmail.com>
2021-08-31 14:21:05 +0200

    Initial commit, imported CSS libraries in project, added fonts, fontawesome and glyphicons in assets folder

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <p>Hello world</p>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+@import url('https://veganmanager.com/src-old/assets/css/bootstrap.min.css');
+@import url('https://veganmanager.com/src-old/assets/css/oneui.min.css');
+@import url('https://veganmanager.com/src-old/assets/css/style-new.css');
+
+#app {
+  color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin-top: 60px;
+  text-align: center;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
