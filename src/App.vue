1ab483c7b85d52ba4d879fe55759475cea9c9c51
ftanevski <tanevski.f98@gmail.com>
2021-09-01 16:37:11 +0200

    Finished task ESSVP1-9

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-    <p>Hello world</p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="dropdown">
+                    <select class="btn btn-default dropdown-toggle"
+                            data-toggle="dropdown"
+                            aria-expanded="false">
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="30">30</option>
+                        <option value="50">50</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-9">
+              <button class="btn btn-secondary">Filter Submissions</button>
+            </div>
+            <div class="col-md-1">
+              <button class="btn btn-primary">Create Submission</button>
+            </div>
+        </div>
+        <div class="row">
+            <entry-table></entry-table>
+        </div>
+        <div class="footerRow justify-content-center">
+            <load-more></load-more>
+        </div>
+    </div>
 </template>
 
 <script>
+import EntryTable from "./components/EntryTable";
+import LoadMore from "./components/LoadMore";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    EntryTable,
+    LoadMore,
+  },
+};
 </script>
 
 <style>
-
-@import url('https://veganmanager.com/src-old/assets/css/bootstrap.min.css');
-@import url('https://veganmanager.com/src-old/assets/css/oneui.min.css');
-@import url('https://veganmanager.com/src-old/assets/css/style-new.css');
+@import url("https://veganmanager.com/src-old/assets/css/bootstrap.min.css");
+@import url("https://veganmanager.com/src-old/assets/css/oneui.min.css");
+@import url("https://veganmanager.com/src-old/assets/css/style-new.css");
 
 #app {
-  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  margin-top: 60px;
-  text-align: center;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
+  font-size: 13px;
+}
+
+.footerRow {
+  margin-bottom: 30px;
+}
+
+.headerRow {
+  display: flex;
+  margin-bottom: 10px;
 }
 </style>
---
+++ src/components/LoadMore.vue
@@ -0,0 +1,11 @@
+<template>
+    <button class="btn btn-primary">Load More</button>
+</template>
+
+<script>
+
+export default {
+    name: 'LoadMore'
+}
+
+</script>
