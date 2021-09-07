db021d46b84df8bf5068e8f660c33d662c54cf16
ftanevski <tanevski.f98@gmail.com>
2021-09-07 10:44:03 +0200

    Implemented feedback, created new component that lists columns in rows to prevent repeatable code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="container-fluid">
-        <div class="row">
+    <div class="container-fluid ">
+        <div class="row headerRow">
             <div class="col-md-1">
                 <div class="dropdown">
-                    <select class="btn btn-default dropdown-toggle"
-                            data-toggle="dropdown"
-                            aria-expanded="false">
+                    <select
+                        class="btn btn-default dropdown-toggle"
+                        data-toggle="dropdown"
+                        aria-expanded="false"
+                    >
                         <option value="10">10</option>
                         <option value="20">20</option>
                         <option value="30">30</option>
@@ -13,51 +15,53 @@
                     </select>
                 </div>
             </div>
-            <div class="col-md-10">
-              <button class="btn btn-secondary">Filter Submissions</button>
+            <div class="col-md-9">
+                <button class="btn btn-secondary">Filter Submissions</button>
             </div>
             <div class="col-md-1">
-              <button class="btn btn-primary">Create Submission</button>
+                <button class="btn btn-primary">Create Submission</button>
             </div>
         </div>
         <div class="row">
-            <entry-table></entry-table>
+            <data-table></data-table>
         </div>
-        <div class="footerRow justify-content-center">
-            <load-more></load-more>
+        <div class="footerRow">
+            <div class="justify-content-center">
+                <button class="btn btn-primary">Load More</button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import EntryTable from "./components/EntryTable";
-import LoadMore from "./components/LoadMore";
+import DataTable from './components/DataTable';
 
 export default {
-  name: "App",
-  components: {
-    EntryTable,
-    LoadMore,
-  },
+    name: 'App',
+    components: {
+        DataTable
+    }
 };
 </script>
 
 <style>
-@import url("https://veganmanager.com/src-old/assets/css/bootstrap.min.css");
-@import url("https://veganmanager.com/src-old/assets/css/oneui.min.css");
-@import url("https://veganmanager.com/src-old/assets/css/style-new.css");
+@import url('https://veganmanager.com/src-old/assets/css/bootstrap.min.css');
+@import url('https://veganmanager.com/src-old/assets/css/oneui.min.css');
+@import url('https://veganmanager.com/src-old/assets/css/style-new.css');
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  font-size: 13px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-size: 13px;
+    padding: 20px;
+    margin: 20px;
 }
 
 .footerRow {
-  margin-bottom: 30px;
+    margin: 20px;
 }
 
 .headerRow {
-  display: flex;
-  margin-bottom: 10px;
+    display: flex;
+    margin-bottom: 10px;
 }
 </style>
---
+++ src/components/DataTableRow.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="row">
+        <div class="colContainer">
+            <data-table-row-columns
+                v-for="(key, value) in row"
+                :title="value"
+                :text="key"
+                :key="key"
+            >
+            </data-table-row-columns>
+            <div class="col-md-2">
+                <p class="text-muted">Action</p>
+                <button class="btn btn-default btn-sm push-5-r">
+                    <i class="fa fa-edit"></i>
+                </button>
+                <button class="btn btn-default btn-sm push-5-r">
+                    <i class="fa fa-eye"></i>
+                </button>
+                <button class="btn btn-default btn-sm push-5-r">
+                    <i class="fa fa-trash"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import DataTableRowColumns from './DataTableRowColumns';
+
+export default {
+    name: 'DataTableRow',
+    components: {
+        DataTableRowColumns
+    },
+    props: {
+		row: {
+			type: Object,
+			required: true,
+            description: 'Objects containing form response information'
+		}
+	},
+};
+</script>
+
+<style>
+.colContainer {
+    align-items: center;
+    display: flex;
+    padding: 20px;
+}
+</style>
