88b5f9df2dcfe6cd8ea1191832319efff8f90e6f
ftanevski <tanevski.f98@gmail.com>
2021-09-27 12:55:57 +0200

    Added comment

--- tests/unit/DataTableHeader.spec.js
+++ tests/unit/DataTableHeader.spec.js
@@ -19,6 +19,9 @@ describe('DataTableHeader', () => {
         wrapper.findAll(dropdownOptionsSelector)[1].element.selected = true;
 
         await wrapper.find(dropdownSelector).trigger('change');
+        // I want to test whether the 'update-posts' event is emitted after changing the value of the dropdown menu. With this I keep
+        // receiving that the received value (emittedEvents) is undefined and I can't see if the event is properly emitted and store as
+        // an array in the emittedEvents variable.
         let emittedEvents = wrapper.emitted('update-posts');
         expect(emittedEvents).toHaveLength(1);
         console.log(emittedEvents);
