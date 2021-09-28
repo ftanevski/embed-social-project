c0a6150f62f62c4ce21f2864b262e0539c0494af
ftanevski <tanevski.f98@gmail.com>
2021-09-28 10:54:25 +0200

    Changed tests from previous task to avoid future conflicts

--- tests/unit/DataTableHeader.spec.js
+++ tests/unit/DataTableHeader.spec.js
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils';
-import DataTableHeader from '../../src/components/DataTable';
+import DataTableHeader from '../../src/components/DataTableHeader';
  
 describe('DataTableHeader', () => {
     let wrapper;
@@ -17,13 +17,12 @@ describe('DataTableHeader', () => {
         let dropdownOptionsSelector = '[data-testid="dropdown-options-test"]';
  
         wrapper.findAll(dropdownOptionsSelector)[1].element.selected = true;
-
         await wrapper.find(dropdownSelector).trigger('change');
-        // I want to test whether the 'update-posts' event is emitted after changing the value of the dropdown menu. With this I keep
-        // receiving that the received value (emittedEvents) is undefined and I can't see if the event is properly emitted and store as
-        // an array in the emittedEvents variable.
+
         let emittedEvents = wrapper.emitted('update-posts');
         expect(emittedEvents).toHaveLength(1);
-        console.log(emittedEvents);
+
+        let expectedPayload = 10;
+        expect(toString(emittedEvents[0][0])).toMatch(toString(expectedPayload))
     });
-})
+});
